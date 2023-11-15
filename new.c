9ad1e40c3f8cc82e2fbfa2c641e74a38c5684b86
Gentle-Jeffrey <geraldinose1.com>
2023-11-15 21:26:23 +0100

    Hello World

--- new.c
+++ new.c
@@ -8,34 +8,33 @@
 #define MAX_COMMAND_LENGTH 100
 
 int main() {
-    char command[MAX_COMMAND_LENGTH];
+	char command[MAX_COMMAND_LENGTH];
 
-    while (1) {
-        printf("#cisfun$ ");
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            printf("\n");
-            exit(0);
-        }
+	while (1) {
+		printf("#cisfun$ ");
+		if (fgets(command, sizeof(command), stdin) == NULL) {
+			printf("\n");
+			break;
+			exit(0);
+		}
+		command[strcspn(command, "\n")] = 0;
 
-        // Remove newline character
-        command[strcspn(command, "\n")] = 0;
+		pid_t pid = fork();
 
-        pid_t pid = fork();
+		if (pid == -1) {
+			perror("fork");
+			exit(1);
+		} else if (pid == 0) {
 
-        if (pid == -1) {
-            perror("fork");
-            exit(1);
-        } else if (pid == 0) {
-            // Child process
-            if (execlp(command, command, (char *)NULL) == -1) {
-                fprintf(stderr, "./shell: %s: No such file or directory\n", command);
-                exit(1);
-            }
-        } else {
-            // Parent process
-            wait(NULL);
-        }
-    }
+			if (execlp(command, command, (char *)NULL) == -1) {
+				fprintf(stderr, "./shell: %s: No such file or directory\n", command);
+				exit(1);
+			}
+		} else {
+			// Parent process
+			wait(NULL);
+		}
+	}
 
-    return 0;
+	return (0);
 }
