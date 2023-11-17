727f70870407c4c710765d90d0bf6b40f9ad484f
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2023-11-16 19:35:59 -0500

    executable shell

--- new.c
+++ new.c
@@ -6,37 +6,23 @@
  * Return: success
  */
 
-<<<<<<< HEAD
 int main(void)
 {
 	char command[MAX_COMMAND_LENGTH];
-
+	
 	while (1)
 	{
 		_printf("#cisfun$ ");
 		if (fgets(command, sizeof(command), stdin) == NULL)
 		{
 			_printf("\n");
-			exit(0);
-		}
-		/*Remove newline character*/
-=======
-int main() {
-	char command[MAX_COMMAND_LENGTH];
-
-	while (1) {
-		printf("#cisfun$ ");
-		if (fgets(command, sizeof(command), stdin) == NULL) {
-			printf("\n");
 			break;
 			exit(0);
 		}
->>>>>>> 9ad1e40c3f8cc82e2fbfa2c641e74a38c5684b86
 		command[strcspn(command, "\n")] = 0;
-
+		
 		pid_t pid = fork();
-
-<<<<<<< HEAD
+		
 		if (pid == -1)
 		{
 			perror("fork");
@@ -46,7 +32,7 @@ int main() {
 		{
 			/*Child process*/
 			if (execlp(command, command, (char *)NULL) == -1)
-				fprintf(stderr, "./shell: %s: No such file or directory\n", command);
+			fprintf(stderr, "./shell: %s: No such file or directory\n", command);
 			exit(1);
 		}
 		else
@@ -54,23 +40,24 @@ int main() {
 			/*Parent process*/
 			wait(NULL);
 		}
-	}
-=======
-		if (pid == -1) {
+		if (pid == -1)
+		{
 			perror("fork");
 			exit(1);
-		} else if (pid == 0) {
-
-			if (execlp(command, command, (char *)NULL) == -1) {
+		}
+		else if (pid == 0)
+		{
+			if (execlp(command, command, (char *)NULL) == -1)
+			{
 				fprintf(stderr, "./shell: %s: No such file or directory\n", command);
 				exit(1);
 			}
-		} else {
-			// Parent process
+		}
+		else
+		{
+			/*Parent process*/
 			wait(NULL);
 		}
 	}
-
->>>>>>> 9ad1e40c3f8cc82e2fbfa2c641e74a38c5684b86
 	return (0);
 }
